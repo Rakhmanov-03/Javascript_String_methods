23dc81252b1787d74675adb07c0d788d29519367
Rakhmanov-03 <omadjonrahmonov588@gmail.com>
2023-11-21 22:10:22 +0500

    4.8-dars vazifa

---
+++ main.js
@@ -0,0 +1,101 @@
+// 31.Oddiy matematik amallar ketma-ketligi string ko’rinishida
+// funksiyaga argument sifatida beriladi. Ushbu funksiya matematik ifodani bajarib natijani qaytarsin.
+// let a=+("2")
+// let b=+("3")
+// alert(a+b)
+// 32.Shunday funksiya yasangki, unda 2ta butun son argument
+// qilib beriladi. Agar ushbu sonlardan birontasi 10ga teng bo’lsa
+// yoki ularning yig’indisi 10ga teng bo’lsa funksiya rost qiymat
+// qaytaradi. Aks hold yolg’on
+// function a(){
+//     let firstnumber=+prompt("1 chi sonni kiriting")
+//     let secondnumber=+prompt("2 chi sonni kiriting")
+//     if(firstnumber==10 || firstnumber+secondnumber==10 ){
+//         alert("True");
+//     }else{
+//         alert("False");
+//     }
+// }
+// a();
+// 33.Mashina kilometriga 10litr benzin ichadi. Mashina doim
+// yo’lga chiqishdan oldin kamida 100litr benzin bilan chiqadi.
+// Agar masofa funksiyaga argument sifatida berilsa, ushbu masofaga chiqish uchun Mashina necha litr benzin bilan chiqishi
+// keraglini funksiya qaytarib bersin.
+// function a(){
+//     let masofa=+prompt("belgilangan yo'lni kiriting")
+//     alert(masofa*10 + " litr benzin ketadi")
+// }
+// a();
+// 34.Quyidagi namunaga nazar tashlagan holda funksiya yasang.
+// Namuna:
+// fun(3, 7) ᔍ 7
+// fun(-1, 0) ᔍ 0
+// fun(1000, 400) ᔍ 1000
+
+// function a(){
+//     let firstnumber=+prompt("birinchi sonni kiriting")
+//     let secondnumber=+prompt("ikkinchi sonni kiriting")
+//     if(firstnumber > secondnumber){
+//         alert(firstnumber)
+//     }else{
+//         alert(secondnumber)
+//     }
+// }
+// a();
+// 36.Funksiyaga 2ta argument sifatida string ko’rinishidagi ma’lumotlar beriladi. Agar ushbu ikkala stringdagi belgilar soni bir
+// birinikiga teng bo’lsa funksiya rost qiymat qaytarsin, aks hold
+// yolg’on
+// function tenglik(){
+//     let firststring=prompt("1-qiymatni kiriting")
+//     let secondstring=prompt("2-qiymatni kiriting")
+//     if(firststring.length === secondstring.length){
+//         consol.log("true")
+//     }else{
+//         consol.log("false")
+//     }
+// }
+// tenglik();
+// 37.Shunday funksiya yasangki, unga string argument qilib
+// beriladi, agar ushbu string bo’sh bo’lsa funksiya true qaytaradi,
+// aks holda false
+// function string(){
+//     let a=prompt("stringni kiriting")
+//    console.log(a.trim() == "");
+// }
+// string();
+// 38.Shunday funksiya yasang.Unda butun son argument qilib
+// beriladi. Agar ushbu butun son 5ga bo’linsa, funksiya true
+// qaytarsin, aks holda false.
+
+// function son(){
+//     let raqam=+prompt("raqamni kiriting")
+//     if(raqam % 5 == 0){
+//         console.log("true")
+//     }else{
+//         console.log("false")
+//     }
+// }
+// son();
+//  39.Shunday funksiya yasang.Unda butun son argument qilib
+// beriladi. Agar ushbu butun son 100ga bo’linsa, funksiya true
+// qaytarsin, aks holda false.
+
+// let son=+prompt("sonni kiriting")
+// if(son % 100 == 0){
+//     alert("true")
+// }else{
+//     alert("false")
+// }
+// 40.Shunday funksiya yasangki, ushbu funksiya stringni ichida nechta belgi borligini aytsin. 
+// Bunda length propertisidan foydalanmang va rekursiv funksiya ishlating.
+//     let soz=prompt("sozni kiriting")
+//     let result=
+// console.log(result);
+
+
+
+
+
+
+
+
